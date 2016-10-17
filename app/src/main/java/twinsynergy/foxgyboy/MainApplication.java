c7cbbff47e3c873d44e32eb85cbfc43c802c5c35
peem-jirayu <phatraphak.piratch@gmail.com>
2016-10-17 09:14:12 +0700

    create project structure

---
+++ app/src/main/java/twinsynergy/foxgyboy/MainApplication.java
@@ -0,0 +1,20 @@
+package twinsynergy.foxgyboy;
+
+import android.app.Application;
+
+/**
+ * Created by peem-jirayu on 17/10/2559.
+ */
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
